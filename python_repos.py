f304107953caca0d9fb2c0a4be07003729b8ab10
space-aliens <vaibhav.choursiya.soc@gmail.com>
2023-11-05 09:22:15 +0530

    printing the keys.

--- python_repos.py
+++ python_repos.py
@@ -12,4 +12,15 @@ print(f"Status code: {r.status_code}")
 responce_dict = r.json()
 
 # Process results.
-print(responce_dict.keys())
+print(f"Total repositories: {responce_dict['total_count']}")
+print(f"Complete results: {not responce_dict['incomplete_results']}")
+
+# Explore information about the repositories.
+repo_dicts = responce_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dicts)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
